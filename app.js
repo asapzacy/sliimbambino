1c0904198665bdef49413320243ee1bef23767e3
Zac Arellano <zacdarellano@gmail.com>
2018-04-15 15:26:52 -0700

    change env variable name for server

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const path = require('path')
 
-const port = process.env.PORT || 4545
+const port = process.env.API_PORT || 4545
 
 const app = express()
 app.use(express.static('./dist'))
